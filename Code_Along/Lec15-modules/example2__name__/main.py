ed29392e9e091a20df8873ec4b8179d727d32d98
Hannesssss <hannes.hedberg@hotmail.se>
2022-10-04 12:07:50 +0200

    Lecture16 Finished

--- Code_Along/Lec15-modules/example2__name__/main.py
+++ Code_Along/Lec15-modules/example2__name__/main.py
@@ -1,9 +1,6 @@
-from ast import Import
-
-from Code_Along.Lec15-modules.example2__name__.module2 import trigonometric_identity
-from module2 Import trigonometric_identity
+from module2 import trigonometric_identity
 
 print(globals()["trigonometric_identity"])
-print(globals()["module2"])
+# print(globals()["module2"])
 
 print(trigonometric_identity(1337))
